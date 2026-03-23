fcf8f6fd362934b0406ac9053daa037629b0b0bb
Santiago Giraldo <santiago.giraldo-c@mail.escuelaing.edu.co>
2026-03-22 19:33:49 -0500

    Create Crypto_Square.cpp

---
+++ Crypto_Square/Crypto_Square.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+string normalize(const string& text) {
+    string result;
+    for (char c : text) {
+        if (isalnum(c)) {
+            result += tolower(c);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+
+    string s = normalize(input);
+    int n = s.size();
+
+    int c = ceil(sqrt(n));
+    int r = floor(sqrt(n));
+    if (r * c < n) r++;
+
+    vector<string> grid(r, string(c, ' '));
+
+    int idx = 0;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (idx < n) {
+                grid[i][j] = s[idx++];
+            }
+        }
+    }
+
+    for (int j = 0; j < c; j++) {
+        for (int i = 0; i < r; i++) {
+            cout << grid[i][j];
+        }
+        cout << " ";
+    }
+
+    return 0;
+}
